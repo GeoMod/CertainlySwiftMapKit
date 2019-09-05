af1a3cd74565a6fdec93f3c66b37e152d2c28016
Dan O'Leary <olearydaniel@me.com>
2019-09-05 17:57:35 -0500

    Initial Commit

---
+++ CertainlyMapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CertainlyMapKit
+//
+//  Created by Daniel O'Leary on 9/5/19.
+//  Copyright © 2019 Impulse Coupled Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
